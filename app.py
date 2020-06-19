64bfe62cb9409ffc6948f04d731bc31777253430
Michelle D Zhang <zhang.michelle.d@gmail.com>
2020-06-18 23:17:45 -0500

    separate

--- app.py
+++ app.py
@@ -1,46 +1,18 @@
 import dash
-from dash.dependencies import Input, Output
-import dash_table
-import dash_core_components as dcc
-import dash_html_components as html
-import pandas as pd
 
-df = pd.read_csv('locations.csv')
+from layout import create_layout, register_callbacks
 
-app = dash.Dash(__name__)
+# for the Local version, import local_layout and local_callbacks
+# from local import local_layout, local_callbacks
 
-app.layout = html.Div([
-    dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": i, "id": i, "deletable": True, "selectable": True} for i in df.columns
-        ],
-        data=df.to_dict('records'),
-        editable=True,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        column_selectable="single",
-        row_selectable="multi",
-        row_deletable=True,
-        selected_columns=[],
-        selected_rows=[],
-        page_action="native",
-        page_current= 0,
-        page_size= 10,
-    ),
-    html.Div(id='datatable-interactivity-container')
-])
-
-@app.callback(
-    Output('datatable-interactivity', 'style_data_conditional'),
-    [Input('datatable-interactivity', 'selected_columns')]
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
-def update_styles(selected_columns):
-    return [{
-        'if': { 'column_id': i },
-        'background_color': '#D2F3FF'
-    } for i in selected_columns]
 
-if __name__ == '__main__':
+server = app.server
+app.layout = create_layout(app)
+register_callbacks(app)
+
+# Running server
+if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ layout.py
@@ -0,0 +1,46 @@
+import dash_html_components as html
+import dash_table
+import pandas as pd
+from dash.dependencies import Input, Output
+
+df = pd.read_csv("locations.csv")
+
+
+def create_layout(app):
+    return html.Div(
+        [
+            dash_table.DataTable(
+                id="datatable-interactivity",
+                columns=[
+                    {"name": i, "id": i, "deletable": True, "selectable": True}
+                    for i in df.columns
+                ],
+                data=df.to_dict("records"),
+                editable=True,
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                column_selectable="single",
+                row_selectable="multi",
+                row_deletable=True,
+                selected_columns=[],
+                selected_rows=[],
+                page_action="native",
+                page_current=0,
+                page_size=10,
+            ),
+            html.Div(id="datatable-interactivity-container"),
+        ]
+    )
+
+
+def register_callbacks(app):
+    @app.callback(
+        Output("datatable-interactivity", "style_data_conditional"),
+        [Input("datatable-interactivity", "selected_columns")],
+    )
+    def update_styles(selected_columns):
+        return [
+            {"if": {"column_id": i}, "background_color": "#D2F3FF"}
+            for i in selected_columns
+        ]
