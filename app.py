92ce306347c33888eddc6cde3e8e611b0e503396
Michelle D Zhang <zhang.michelle.d@gmail.com>
2020-06-18 23:14:51 -0500

    simple data table

---
+++ app.py
@@ -0,0 +1,46 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv('locations.csv')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dash_table.DataTable(
+        id='datatable-interactivity',
+        columns=[
+            {"name": i, "id": i, "deletable": True, "selectable": True} for i in df.columns
+        ],
+        data=df.to_dict('records'),
+        editable=True,
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        column_selectable="single",
+        row_selectable="multi",
+        row_deletable=True,
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current= 0,
+        page_size= 10,
+    ),
+    html.Div(id='datatable-interactivity-container')
+])
+
+@app.callback(
+    Output('datatable-interactivity', 'style_data_conditional'),
+    [Input('datatable-interactivity', 'selected_columns')]
+)
+def update_styles(selected_columns):
+    return [{
+        'if': { 'column_id': i },
+        'background_color': '#D2F3FF'
+    } for i in selected_columns]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
