a387a42fefab36255212d03277fb417e447219ea
Michelle Zhang <michelle@hackerone.com>
2020-07-19 15:04:21 -0500

    fixes

--- txdps/config.py
+++ txdps/config.py
@@ -3,10 +3,19 @@ import logging
 import os
 
 import sentry_sdk
-from sentry_sdk.integrations.flask import FlaskIntegration
 
 
 def setup():
     """Set up logging and error reporting."""
     logging.basicConfig(level=logging.INFO)
-    sentry_sdk.init(dsn=os.environ["SENTRY_DSN"], integrations=[FlaskIntegration()])
+
+    integrations = []
+
+    try:
+        from sentry_sdk.integrations.flask import FlaskIntegration
+
+        integrations.append(FlaskIntegration())
+    except sentry_sdk.integrations.DidNotEnable as exc:
+        logging.warn(exc)
+
+    sentry_sdk.init(dsn=os.environ["SENTRY_DSN"], integrations=integrations)
